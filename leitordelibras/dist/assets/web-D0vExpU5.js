dd15b9ea33f24dc40c045a7856daa412885fb0d5
Carlos Antônio <148822990+tonhob12@users.noreply.github.com>
2024-10-03 21:19:54 -0300

    Add files via upload

---
+++ leitordelibras/dist/assets/web-D0vExpU5.js
@@ -0,0 +1,12 @@
+import { W as WebPlugin } from "./index-DUJpA7IY.js";
+class SplashScreenWeb extends WebPlugin {
+  async show(_options) {
+    return void 0;
+  }
+  async hide(_options) {
+    return void 0;
+  }
+}
+export {
+  SplashScreenWeb
+};
